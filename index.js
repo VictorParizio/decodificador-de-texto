3c5500a819f7afeb686ba2f5c9d43e6d478d63d9
Victor Parizio <victorparizio@hotmail.com>
2024-01-27 22:29:22 -0300

    feat (index.js) Foi criada a lógica para descriptografar texto

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const text = document.getElementById("text");
 const encryptBtn = document.getElementById("encrypt-button");
+const decryptBtn = document.getElementById("decrypt-button");
 
 const encryptedText = () => {
   const arrayLetters = text.value.split("");
@@ -25,4 +26,38 @@ const encryptedText = () => {
   alert(result);
 };
 
+const decryptedText = () => {
+  const arrayWords = text.value.split(" ");
+  const keyWords = ["ai", "enter", "imes", "ober", "ufat"];
+  let encryptedText = [];
+
+  for (word of arrayWords) {
+    for (keyWord of keyWords) {
+      while (word.includes(keyWord)) {
+        if (word.includes("ai")) {
+          word = word.replace("ai", "a");
+        }
+        if (word.includes("enter")) {
+          word = word.replace("enter", "e");
+        }
+        if (word.includes("imes")) {
+          word = word.replace("imes", "i");
+        }
+        if (word.includes("ober")) {
+          word = word.replace("ober", "o");
+        }
+        if (word.includes("ufat")) {
+          word = word.replace("ufat", "u");
+        }
+      }
+    }
+
+    encryptedText.push(word);
+  }
+
+  const result = encryptedText.join(" ");
+  alert(result);
+};
+
 encryptBtn.addEventListener("click", encryptedText);
+decryptBtn.addEventListener("click", decryptedText);
